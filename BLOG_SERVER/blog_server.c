eedfde2aa0e98eea917db1715426024eb07ded38
shug huang <shughuang@shugdeMacBook-Pro.local>
2018-03-28 15:47:52 +0800

    pthread_add

--- BLOG_SERVER/blog_server.c
+++ BLOG_SERVER/blog_server.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+6#include<stdio.h>
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<unistd.h>
--- BLOG_SERVER/pthread_task_deal.c
+++ BLOG_SERVER/pthread_task_deal.c
@@ -10,7 +10,7 @@
 #define MIN_PTHREAD_NUM 5
 #define MAX_PTHREAD_NUM 100
 #define MAX_TASK_NUM 100
-
+#define DEFAULT_ADD 5
 typedef struct node
 {
 	pthread_t *tids;
@@ -22,6 +22,7 @@ typedef struct node
 	int busy_thread;
 	int live_thread;
 	int exit_thread;
+	int wait_thread;
 	int task_queue_tail;
 	int task_queue_front;
 	int task_num;
@@ -44,6 +45,10 @@ void *pthread_fun(void *argv)
 		pthread_mutex_lock(&m_pool->thread_pool_data_lock);
 		if(m_pool->task_num==0)
 		{
+			pthread_mutex_lock(&m_pool->thread_pool_count_lock);
+			m_pool->wait_thread++;
+			pthread_mutex_unlock(&m_pool->thread_pool_count_lock);
+
 			pthread_cond_wait(&mpool->task_queue_not_empty_lock,&thread_pool_data_lock);
 		}
 		if(m_pool->is_shutdown==true)
@@ -73,10 +78,81 @@ void *pthread_fun(void *argv)
 	}
 	
 }
+int is_ALIVE(pid_t tid)
+{
+
+		int kill_rc = pthread_kill(tid, 0);
+	if (kill_rc == ESRCH)
+		return false;
+
+	return true;
+}
 void *pthread_adjust_fun(void *argv)
 {
 pthread_pool_data* m_pool=(pthread_pool_data*)argv;
 
+
+while (true)
+{
+sleep(10);
+pthread_mutex_lock(&m_pool->thread_pool_data_lock);
+
+int live_thread_num=m_pool->live_thread;
+int queue_size=m_pool->task_num;
+pthread_mutex_unlock(&m_pool->thread_pool_data_lock);
+pthread_mutex_lock(&m_pool->thread_pool_count_lock);
+int busy_thread_num=m_pool->busy_thread;
+pthread_mutex_lock(&m_pool->thread_pool_count_lock);
+
+
+int add;
+if(live_thread_num*0.8<busy_thread_num&&live_thread<MAX_PTHREAD_NUM)
+{
+	if(live_thread_num<MAX_PTHREAD_NUM-DEFAULT_ADD)
+	{
+		add=DEFAULT_ADD;
+	}
+	else
+	{
+		printf("thread is full");
+
+	}
+pthread_mutex_lock(&m_pool->thread_pool_data_lock);
+int i,g;
+for(i=0,g=0;i<MAX_PTHREAD_NUM&&g<add;i++)
+{
+	if(is_ALIVE(m_pool->tids[i]))
+		continue;
+	else if(m_pool->tids[i]==0||!is_ALIVE(m_pool->tids[i]))
+	{
+	pthread_create(m_pool->tids[i],0,pthread_fun,(void *)m_pool);	
+	}
+
+
+	m_pool->live_thread+=DEFAULT_ADD;
+
+
+}
+pthread_mutex_unlock(&m_pool->thread_pool_data_lock);
+
+if(live_thread_num>busy_thread_num*2&&live_thread_num>MIN_PTHREAD_NUM)
+{
+	int i;
+	pthread_mutex_lock(&m_pool->thread_pool_data_lock);
+	m_pool->exit_thread+=DEFAULT_ADD;
+
+	pthread_mutex_unlock(&m_pool->thread_pool_data_lock);
+
+	for(i=0;i<DEFAULT_ADD;i++)
+	{
+		pthread_cond_signal(&m_pool->task_queue_not_full_lock);
+	}
+
+}
+
+
+}
+}
 }
 pthread_pool_data *create_pthread_pool()
 {
@@ -96,6 +172,7 @@ pthread_pool_data *create_pthread_pool()
 	m_pthread_pool->task_queue_front=0;
 	m_pthread_pool->task_queue_tail=0;
 	m_pthread_pool->task_num=0;
+	m_pthread_pool->wait_thread=0;
 	int i;
 	for(i=0;i<MIN_PTHREAD_NUML;i++)
 	{
